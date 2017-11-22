1978f1d8223ec94d8adc74f4efaa07d5de648296
mitch <mitchell.francisco1@gmail.com>
2017-11-21 17:19:06 -0800

    Format data for processing

--- server.R
+++ server.R
@@ -9,3 +9,9 @@ data$year <- data$year + 2004
 data <- data %>% select(-records_lost) %>% rename(records_lost = records_lost_v2)
 # Could display a quick "headline" story about one of these data breaches listed as "interesting"?
 top.stories <- data %>% filter(interesting == "y") %>% select(description)
+# Convert data_sensitivity column from integers to specific strings
+data$data_sensitivity[data$data_sensitivity == 1] <- "General online info"
+data$data_sensitivity[data$data_sensitivity == 20] <- "Sensitive PII"
+data$data_sensitivity[data$data_sensitivity == 300] <- "Credit card info"
+data$data_sensitivity[data$data_sensitivity == 4000] <- "Email credentials, health records"
+data$data_sensitivity[data$data_sensitivity == 50000] <- "Bank account details"
