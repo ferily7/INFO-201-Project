f0af119a5be584e144fa5a02947d4c3ff51c5ed9
Farrah Lee <farrahyglee@yahoo.com>
2017-11-24 01:11:03 -0800

    created selectbox

--- server.R
+++ server.R
@@ -4,3 +4,19 @@ library(plotly)
 library(ggplot2)
 
 source('dataset.R')
+
+shinyServer(function(input, output) {
+  
+  # Create bar graph of the cyber breaches based on the type of organization
+  output$selectBox <- renderPlotly({
+    # Filtering data by input choice
+    type.of.org <- breaches  %>%  
+      filter(type_org == input$choice)
+    
+    # Make Bar graph
+    plot_ly(x = type.of.org$records_lost, y = type.of.org$entity_name, type = 'bar') %>% 
+      layout(margin = list(l = 300))
+  })
+  
+})
+
--- ui.R
+++ ui.R
@@ -1,3 +1,24 @@
 library(shiny)
 library(plotly)
 library(ggplot2)
+
+shinyUI(fluidPage(
+  
+  # Application title
+
+  # Sidebar with a slider input for number of calories 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("choice", label = "Choose the type of organization", 
+                  choices = list("Web" = "web", "Financial" = "financial", "Telecoms" = "telecoms", "Tech" = "tech", "Government" = "government", 
+                                 "Retail" = "retail", "Energy" = "energy", "Academic" = "academic", "Healthcare" = "healthcare", "Military" = "military",
+                                 "Gaming" = "gaming", "Media" = "media", "Transport" = "transport", "Legal" = "legal", "App" = "app"), 
+                  selected = 1)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotlyOutput("selectBox")
+    )
+  )
+))
