f1d89910ee1b3e9b6519d72b8e2a630fff9d233b
Farrah Lee <farrahyglee@yahoo.com>
2017-11-30 23:58:03 -0800

    edited intro paragraph

--- ui.R
+++ ui.R
@@ -24,26 +24,29 @@ my.ui <- navbarPage(
           tags$a(href=story$source_2, "(additional source)")
         }
       ),
+      
       mainPanel(
         # All of this text needs editing
         tags$h2("What is a data breach?"),
-        tags$p("A data breach occurs when an unauthorized entity has stolen, copied, 
-               or viewed data that is private, confidential or protected."),
-        tags$h2("What are examples of data breaches?"),
-        tags$p("A common example of a data breach is some malicious user hacking into
-               a unauthorized network and stealing data files. However, data breaches
-               can occur simply an employee leaving confidential information up on their
-               computer screen, and then some unauthorized employee is able to read it."),
+        tags$p("A data breach is when private, confidential or protected data has 
+               been stolen, copied or viewed by an unauthorized entity."),
+        tags$h2("What are examples of data breaches and how bad can they be?"),
+        tags$p("A common example of a data breach is when some malicious user hacks into
+               an unauthorized network and steals data files. However, data breaches can
+               also occur simply when an employee leaves confidential information up on their
+               computer screen and some unauthorized employee is able to read it. One of the most
+               recent security breach that occurred was the Equifax Security Leak. Because of the 
+               data breach, it impact more than 2.5 million Americans, which included information 
+               such as social security numbers, addresses, driver license numbers and credit card 
+               information."),
         tags$h2("Why are data breaches bad? Who does it impact?"),
-        tags$p("Obviously, when a company is hit with a data breach, they inevitably lose revenue.
-               Often they will be hit by the press, and end up being painted in a negative light.",
-               tags$br(),
-               "But this is not why a data breach can be devastating. What if the data that was stolen
-               included your personal information? Now, private information is no longer private, and
-               can end up being sold and your privacy violated. When a company is hit by a data breach,
-               especially when the company handles sensitive information such as bank accounts, passwords,
-               or health records, all consumers are impacted.")
-        
+        tags$p("Obviously, when a company is hit by a data breach, they inevitably lose revenue.
+               Often they will be hit by the press and end up being painted in a negative light.
+               However, this is not why a data breach can be devastating. What if the data that 
+               was stolen included your personal information? Now, private information is no longer 
+               private and can end up being sold, with your privacy violated. When a company is hit 
+               by a data breach, especially when the company handles sensitive information such as 
+               bank accounts, passwords, or health records, all consumers are impacted.")
       )
     )
   ),
